a2d62e98ff3dab4c9f569f7246e015a019eecda6
Mauro <maurozirpoli7@gmail.com>
2024-03-17 18:22:23 -0300

    otro cambio

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -14,7 +14,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hola TODO");
+       System.out.println("Hola TOD");
     }
     
 }
