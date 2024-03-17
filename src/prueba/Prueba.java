1e9e387e9c396e28671148f5d187e4d42a7f5d9c
Mauro <maurozirpoli7@gmail.com>
2024-03-17 18:40:32 -0300

    modifique en rama maurito

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -14,7 +14,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hola TOD");
+       System.out.println("Hola TODRRR");
     }
     
 }
